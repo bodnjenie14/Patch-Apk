1971efae8230ce857b36dd2239aa6e0c6c4c9389
Glitched Panda <82221876+GlitchedPanda@users.noreply.github.com>
2025-01-07 18:47:52 +0000

    Made dlc url patching work and removed windows version

--- patch-apk.sh
+++ patch-apk.sh
@@ -7,6 +7,9 @@ INPUT_FILENAME=$1
 NEW_GAMESERVER_URL=$2
 NEW_DLCSERVER_URL=$3
 
+export DLC_URL="${NEW_DLCSERVER_URL}"
+export SOURCE_OUTPUT="tappedout"
+
 if [ -z "${INPUT_FILENAME}" ] || [ -z "${NEW_GAMESERVER_URL}" ] ||  [ -z "${NEW_DLCSERVER_URL}" ]; then
     echo "Usage:" $0 "[INPUT_FILENAME] [NEW_GAMESERVER_URL] [NEW_DLCSERVER_URL]"
     exit 1
@@ -29,6 +32,11 @@ if ! [ -f venv/bin/pip ]; then
 	if [ ! $? -eq 0 ]; then
 	    exit 1
 	fi
+
+	venv/bin/pip install r2pipe
+	if [ ! $? -eq 0 ]; then
+	    exit 1
+	fi
 fi
 
 ######################################
@@ -41,9 +49,11 @@ java -jar apktool_2.10.0.jar  d $INPUT_FILENAME -o tappedout
 ######################################
 
 grep -rl "https://prod.simpsons-ea.com" ./tappedout/ 	| xargs -r sed -i "s|https://prod.simpsons-ea.com|${NEW_GAMESERVER_URL}|g" 	# Gameserver 
+
 grep -rl "https://syn-dir.sn.eamobile.com" ./tappedout/ | xargs -r sed -i "s|https://syn-dir.sn.eamobile.com|${NEW_GAMESERVER_URL}|g" 	# Director Api Url
 
 grep -rl "https://oct2018-4-35-0-uam5h44a.tstodlc.eamobile.com/netstorage/gameasset/direct/simpsons/" ./tappedout/ | xargs -r sed -i "s|https://oct2018-4-35-0-uam5h44a.tstodlc.eamobile.com/netstorage/gameasset/direct/simpsons/|${NEW_DLCSERVER_URL}|g" # DLC Server
+venv/bin/python3 patch_native.py # DLC Server
 
 ######################################
 #           Recompile App            #
